8a7996c8a3f6ed76c774afb85ed417cc0459468e
Andrew Pisliakov <pislakov@gmail.com>
2022-05-05 21:59:19 +0300

    feat: add language location

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let body = document.querySelector('body');
+console.log(body);
+
+let h1 = document.createElement('h1');
+h1.innerHTML = "RSS Виртуальная клавиатура";
+body.append(h1);
+h1.classList.add('main-title');
